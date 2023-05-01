2c52b21397976762fb16e999245074d7bfaab692
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2023-05-01 17:59:24 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -27,7 +27,7 @@ class TestConsoleDocs(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
     def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
+        """Test for the console.py module docstring """
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
